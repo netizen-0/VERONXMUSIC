f9abee5897e5cb475f07d09c893321f71454dfb2
netizen-0 <rishabhytpremium17@gmail.com>
2025-11-14 18:11:21 +0530

    Update Youtube.py

--- ANNIEMUSIC/platforms/Youtube.py
+++ ANNIEMUSIC/platforms/Youtube.py
@@ -9,7 +9,7 @@ from typing import Dict, List, Optional, Tuple, Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 from ANNIEMUSIC.utils.cookie_handler import COOKIE_PATH
 from ANNIEMUSIC.utils.database import is_on_off
